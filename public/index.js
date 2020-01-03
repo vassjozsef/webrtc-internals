b5ec384a526dcb461586c230e4b28f4e9d11c985
Jozsef Vass <jozsef@discordapp.com>
2020-01-02 18:36:02 -0800

    use just minimal

--- public/index.js
+++ public/index.js
@@ -1,12 +1,4 @@
-const graphViewId = 'test';
-const divId = graphViewId + '-div';
-const canvasId = graphViewId + '-canvas';
-const container = document.createElement("div");
-const topContainer = $('content-root');
-topContainer.appendChild(container);
-const statsName = 'Test Graph';
-container.innerHTML = '<div>' + statsName + '</div>' + '<div id=' + divId + '><canvas id=' + canvasId + '></canvas></div>';
-const graphView = new TimelineGraphView(divId, canvasId);
+const graphView = new TimelineGraphView('graph-div', 'graph-canvas');
 
 const dataSeries = new TimelineDataSeries();
 graphView.addDataSeries(dataSeries);
